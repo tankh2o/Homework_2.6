fb9763b0daf0e9a3c02785424282fd47e67a61d2
TanY <tankh2o@yandex.ru>
2023-06-16 15:06:19 +0800

    1

--- src/main/java/com/example/Homework26/EmployeeServiceInterface.java
+++ src/main/java/com/example/Homework26/EmployeeServiceInterface.java
@@ -3,16 +3,6 @@ package com.example.Homework26;
 import java.util.List;
 
 public interface EmployeeServiceInterface {
-    //### Шаг 5
-    //    //Реализовать в сервисе три метода, которые принимают в качестве параметров firstName и lastName:
-    //    //1. Добавить сотрудника.
-    //    //2. Удалить сотрудника. remove(int index)
-    //    //3. Найти сотрудника. contains(element) ИЛИ
-
-
-    //### Шаг 7
-    //Написать собственное непроверяемое исключение EmployeeStorageIsFullException,
-    // которое выбрасывается, если превышен лимит количества сотрудников в фирме.
     List<Employee> findOutNumberEmployees();
 
     Employee findEmployees(String firstName, String lastName) throws EmployeeStorageIsFullException;
